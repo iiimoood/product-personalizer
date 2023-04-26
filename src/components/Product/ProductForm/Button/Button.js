5e0279d826d47c889cea4297f57647d4a1a28e54
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-04-26 17:53:50 +0200

    Add fix in button component

--- src/components/Product/ProductForm/Button/Button.js
+++ src/components/Product/ProductForm/Button/Button.js
@@ -2,9 +2,9 @@ import styles from '../../Product.module.scss';
 
 const Button = (props) => {
   return (
-    <Button className={styles.button}>
+    <button className={styles.button}>
       <span className="fa fa-shopping-cart" />
-    </Button>
+    </button>
   );
 };
 
